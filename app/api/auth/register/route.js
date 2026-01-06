8d30aa49a1a3a92c296166ec73dc40b4a0d3752c
rimu-7 <rrimu99@gmail.com>
2026-01-06 14:59:47 +0800

    register/route

--- app/api/auth/register/route.js
+++ app/api/auth/register/route.js
@@ -1,8 +1,9 @@
 import { NextResponse } from "next/server";
 import bcrypt from "bcryptjs";
 import connectDB from "@/lib/mongodb";
-import User from "@/lib/schema";
+
 import { signToken } from "@/lib/jwt";
+import { User } from "@/lib/schema";
 
 
 function jsonError(message, status = 400) {
